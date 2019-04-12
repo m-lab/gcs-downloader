7f76415221b3d11604f033eac0114a91326b04ec
Stephen Soltesz <soltesz@google.com>
2019-04-12 17:07:51 -0400

    Use ioutil.TempDir for test files

--- main_test.go
+++ main_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"testing"
 
 	"github.com/dchest/safefile"
@@ -16,6 +17,10 @@ func init() {
 }
 
 func Test_main(t *testing.T) {
+	dir, err := ioutil.TempDir("", "gcs-downloader-main-")
+	if err != nil {
+		t.Fatal(err)
+	}
 	tests := []struct {
 		name     string
 		source   string
@@ -24,40 +29,40 @@ func Test_main(t *testing.T) {
 		{
 			name:     "ok",
 			source:   "gs://m-lab-gcs-downloader-mlab-testing/t1/okay.txt",
-			tempFile: atomicfile.New("/tmp/junk"),
+			tempFile: atomicfile.New(path.Join(dir, "junk")),
 		},
 		{
 			name:     "error",
 			source:   "gs://m-lab-gcs-downloader-mlab-testing/t1/okay.txt",
-			tempFile: fakeNew("/tmp/junk", fmt.Errorf("fake temp file create failed")),
+			tempFile: fakeNew(path.Join(dir, "junk"), fmt.Errorf("fake temp file create failed")),
 		},
 	}
 
 	for _, tt := range tests {
 		once = true // Only run once.
 		source = tt.source
-		tempNew = func(name string) atomicfile.File { return tt.tempFile }
+		atomicfileNew = func(name string) atomicfile.File { return tt.tempFile }
 		t.Run(tt.name, func(t *testing.T) {
 			main()
 		})
 	}
 }
 
-type fakeTemp struct {
+type fakeAtomicfile struct {
 	name        string
 	createError error
 }
 
 func fakeNew(name string, createError error) atomicfile.File {
-	return &fakeTemp{name: name, createError: createError}
+	return &fakeAtomicfile{name: name, createError: createError}
 }
 
-func (f *fakeTemp) Create(perm os.FileMode) (*safefile.File, error) {
+func (f *fakeAtomicfile) Create(perm os.FileMode) (*safefile.File, error) {
 	return &safefile.File{}, f.createError
 }
-func (f *fakeTemp) Commit() error {
+func (f *fakeAtomicfile) Commit() error {
 	return nil
 }
-func (f *fakeTemp) Name() string {
+func (f *fakeAtomicfile) Name() string {
 	return f.name
 }
